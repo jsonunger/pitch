94c70f584969eb098488306673bb1cca144843ac
Jason Unger <jasonscottunger@gmail.com>
2016-09-07 18:28:16 -0400

    Songlist and scroll width implementation

---
+++ browser/js/action-reducers/scrollWidth.js
@@ -0,0 +1,12 @@
+const SET_SCROLL_WIDTH = 'SET_SCROLL_WIDTH';
+
+export const setScrollWidth = scrollWidth => ({ type: SET_SCROLL_WIDTH, scrollWidth });
+
+export default function currentProgress (state = 0, action) {
+  switch (action.type) {
+    case SET_SCROLL_WIDTH:
+      return action.scrollWidth;
+    default:
+      return state;
+  }
+}
---
+++ browser/js/containers/SongListContainer.js
@@ -0,0 +1,24 @@
+import { connect } from 'react-redux';
+import SongList from '../components/SongList';
+import { start } from '../action-reducers/playerActions';
+import { removeSong } from '../action-reducers/playlist';
+
+function mapStateToProps (state) {
+  return {
+    currentSong: state.currentSong,
+    playlist: state.playlist
+  };
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    playSong(song, list) {
+      return dispatch(start(song, list));
+    },
+    removeSong (playlistId, songId) {
+      return dispatch(removeSong(playlistId, songId));
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SongList);

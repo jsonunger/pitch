40cfba4413078318874dae9c34c0c2ed9b6a1136
Jason Unger <jasonscottunger@gmail.com>
2016-09-30 01:29:30 -0400

    Fixed side effect of webpack middleware file configuration

--- server/app/configure/webpack-middleware.js
+++ server/app/configure/webpack-middleware.js
@@ -1,15 +1,11 @@
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import config from '../../../webpack.dev.config';
-
 export default function (app) {
   if (process.env.NODE_ENV !== 'production') {
-    const compiler = webpack(config);
-    app.use(webpackDevMiddleware(compiler, {
+    const config = require('../../../webpack.dev.config.js');
+    const compiler = require('webpack')(config);
+    app.use(require('webpack-dev-middleware')(compiler, {
       noInfo: true,
       publicPath: config.output.publicPath
     }));
-    app.use(webpackHotMiddleware(compiler));
+    app.use(require('webpack-hot-middleware')(compiler));
   }
 }

e004a1b50c65ea73fe8fb82efdb946e67f3798e1
Jason Unger <jasonscottunger@gmail.com>
2016-10-26 01:02:40 -0400

    Integrated with GA

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,21 @@ import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { syncHistoryWithStore } from 'react-router-redux';
+import ReactGA from 'react-ga';
 import buildRoutes from './routes';
 import store from './store';
 
 const history = syncHistoryWithStore(browserHistory, store);
+ReactGA.initialize('UA-86348405-2');
+
+function logPageView () {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+}
 
 const Juke = () => (
   <Provider store={store}>
-    <Router routes={buildRoutes(store)} history={history} />
+    <Router routes={buildRoutes(store)} history={history} onUpdate={logPageView} />
   </Provider>
 );
 
